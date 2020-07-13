70fdcead736efc8f3da2b03a4c477149b29e2cb6
IgorBezusenko <bezusenko.98@mail.ru>
2020-07-14 00:06:16 +0300

    got data with server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!!");
+const getResourse = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResourse("https://swapi.dev/api/people/1/").then((body) => {
+  console.log(body);
+});
