63446c5ca3dc4cfb687b9a2e5ff3f607b5e14ad2
IgorBezusenko <bezusenko.98@mail.ru>
2020-07-14 00:22:06 +0300

    added fetch error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const getResourse = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `received ${res.status}`);
+  }
   const body = await res.json();
   return body;
 };
 
-getResourse("https://swapi.dev/api/people/1/").then((body) => {
-  console.log(body);
-});
+getResourse("https://swapi.dev/api/people/1/")
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error("Could not fetch", err);
+  });
