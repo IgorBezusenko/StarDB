9dd47729e94e2365f527d3b8279c9ff592890bd7
IgorBezusenko <bezusenko.98@mail.ru>
2020-07-15 13:48:06 +0300

    added markup of project

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
-class SwapiService {
-  _apiBase = "https://swapi.dev/api";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  async getResourse(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` + `received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResourse(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResourse(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResourse(`/planets/`);
-    return res.results;
-  }
-
-  getPlanets(id) {
-    return this.getResourse(`/planets/${id}`);
-  }
-
-  async getAllStarShips() {
-    const res = await this.getResourse(`/starships/`);
-    return res.results;
-  }
-
-  getStarShips(id) {
-    return this.getResourse(`/starships/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-swapi.getPerson(3).then((p) => {
-  console.log(p.name);
-});
+ReactDOM.render(<App />,
+  document.getElementById('root'));
