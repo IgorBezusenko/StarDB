1b0de3e848eb5cba169bf149838140b790320c26
IgorBezusenko <bezusenko.98@mail.ru>
2020-07-19 12:51:54 +0300

    added Error Massage

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./error-indicator.css";
+import icon from "./death-star.png";
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <img src={icon} alt="error icon" />
+      <span className="boom">OOh, Error!</span>
+      <span>something has gone terribly wrong</span>
+      <span>(but we already sent droids to fix it)</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import SwapiService from "../../services/swapi-service";
 import "./random-planet.css";
 import Spinner from "../spinner/spinner";
+import ErrorIdicator from "../error-indicator";
+import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
-  state = { planet: {}, loading: true };
+  state = { planet: {}, loading: true, error: false };
 
   constructor() {
     super();
@@ -17,19 +19,30 @@ export default class RandomPlanet extends Component {
     this.setState({ planet, loading: false });
   };
 
+  onError = (err) => {
+    this.setState({ error: true, loading: false });
+  };
+
   updatePlanet() {
-    const id = 10;
-    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+    const id = 15000;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
